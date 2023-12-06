f75463a03cdce5ef77b1c55899cc26af5bce8ad6
ttiimmothy <dinosauli2006@gmail.com>
2023-12-06 02:47:49 -0500

    feat: 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,69 @@
-use std::net::UdpSocket;
+use std::{
+  env,
+  net::{Ipv4Addr, SocketAddr, UdpSocket},
+};
+use bytes::{Bytes, BytesMut};
+use message::Message;
+use nom::Offset;
+use parser::{expand_answer, expand_question};
+mod message;
+mod parser;
+
+fn handleDataGram(received_data: Bytes, source: SocketAddr, udp_socket: &UdpSocket) {
+  eprintln!("received data: {:02X?}", received_data);
+  let mut message = Message::from(&received_data[..]);
+  let questions = message.expanded_questions();
+  if let Some(addr) = env::args().nth(2) {
+    for question in questions {
+      let mut forward_message = Message::from(&received_data[..12]);
+      forward_message.set_question_count(0);
+      forward_message.add_question(&question);
+      message.add_answer(&forward_question(forward_message, &addr));
+    }
+  } else {
+    for question in questions {
+      message.answer_question(&question, 60, &Ipv4Addr::new(8, 8, 8, 8).octets())
+    }
+  }
+  message.set_response();
+  if message.opcode() == 0 {
+    message.set_rcode(0);
+  } else {
+    message.set_rcode(4);
+  }
+  eprintln!("response: {:02X?}", message);
+  udp_socket.send_to(&message, source).expect("Failed to send response");
+}
+
+fn forward_question(message: Message, addr: &str) -> BytesMut {
+  let socket = UdpSocket::bind("127.0.0.1:0").expect("failed to bind to a new system assigned port");
+  socket.connect(addr).unwrap_or_else(|e| panic!("error connecting to resolver: {e}"));
+  socket.send(&message).unwrap_or_else(|e| panic!("error sending message: {e}"));
+  let mut buf = [0u8; 512];
+  match socket.recv(&mut buf) {
+    Ok(size) => {
+      let response = &buf[..size];
+      let (r, _) = expand_question(response, 12).unwrap_or_else(|e| panic!("expand question error: {e}"));
+      let (_, answer) = expand_answer(response, response.offset(r)).unwrap_or_else(|e| panic!("expand answer error: {e}"));
+      answer
+    }
+    Err(e) => panic!("receive from resolver: {e}"),
+  }
+}
 
 fn main() {
-    let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    let mut buf = [0; 512];
-    
-    loop {
-        match udp_socket.recv_from(&mut buf) {
-            Ok((size, source)) => {
-                let _received_data = String::from_utf8_lossy(&buf[0..size]);
-                println!("Received {} bytes from {}", size, source);
-                let response = [];
-                udp_socket
-                    .send_to(&response, source)
-                    .expect("Failed to send response");
-            }
-            Err(e) => {
-                eprintln!("Error receiving data: {}", e);
-                break;
-            }
-        }
+  let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
+  let mut buf = [0; 512];
+  loop {
+    match udp_socket.recv_from(&mut buf) {
+      Ok((size, source)) => {
+        println!("Received {} bytes from {}", size, source);
+        handleDataGram(Bytes::copy_from_slice(&buf[..size]), source, &udp_socket);
+      }
+      Err(e) => {
+        eprintln!("Error receiving data: {}", e);
+        break;
+      }
     }
+  }
 }
---
+++ src/message.rs
@@ -0,0 +1,123 @@
+#![allow(dead_code)]
+use std::{
+  fmt::Debug,
+  iter::once,
+  ops::{Deref, DerefMut},
+};
+
+use bytes::{BufMut, BytesMut};
+use nom::Offset;
+use crate::parser::{expand_question, parse_domains};
+
+const HEADER_LENGTH: usize = 12;
+
+pub struct Message(BytesMut);
+impl Message {
+  pub fn new() -> Self {
+    Self(BytesMut::zeroed(HEADER_LENGTH))
+  }
+  pub fn id(&self) -> u16 {
+    u16::from_be_bytes([self[0], self[1]])
+  }
+  pub fn set_id(&mut self, id: u16) {
+    self[..2].copy_from_slice(&id.to_be_bytes());
+  }
+  pub fn set_query(&mut self) {
+    self[2] &= 0b0111_1111;
+  }
+  pub fn set_response(&mut self) {
+    self[2] |= 0b1000_0000;
+  }
+  pub fn opcode(&self) -> u8 {
+    self[2] << 1 >> 4
+  }
+  pub fn rd(&self) -> u8 {
+    self[2] & 0b0000_0001
+  }
+  pub fn rcode(&self) -> u8 {
+    self[3] & 0b0000_1111
+  }
+  pub fn set_rcode(&mut self, rcode: u8) {
+    self[3] = (self[3] & 0b1111_0000) | (0b0000_1111 & rcode)
+  }
+  pub fn question_count(&self) -> u16 {
+    u16::from_be_bytes([self[4], self[5]])
+  }
+  pub fn set_question_count(&mut self, count: u16) {
+    self[4..6].copy_from_slice(&count.to_be_bytes());
+  }
+  pub fn answer_count(&self) -> u16 {
+    u16::from_be_bytes([self[6], self[7]])
+  }
+  pub fn set_answer_count(&mut self, count: u16) {
+    self[6..8].copy_from_slice(&count.to_be_bytes());
+  }
+  pub fn original_questions(&self) -> Vec<&[u8]> {
+    let (_, questions) = parse_domains(&self[HEADER_LENGTH..], self.question_count() as usize).unwrap();
+    questions
+  }
+  
+  pub fn expanded_questions(&self) -> Vec<BytesMut> {
+    let mut res = Vec::new();
+    let mut qc = self.question_count();
+    let mut offset = HEADER_LENGTH;
+    loop {
+      let (r, question) = expand_question(self, offset).unwrap();
+      res.push(question);
+      qc -= 1;
+      if qc == 0 {
+        break;
+      }
+      offset = (self).offset(r);
+    }
+    res
+  }
+
+  pub fn add_question(&mut self, question: &[u8]) {
+    assert_eq!(self.answer_count(), 0);
+    self.put(question);
+    self.set_question_count(self.question_count() + 1);
+  }
+
+  pub fn add_answer(&mut self, answer: &[u8]) {
+    self.put(answer);
+    self.set_answer_count(self.answer_count() + 1);
+  }
+  pub fn answer_question(&mut self, question: &[u8], ttl: u32, data: &[u8]) {
+    self.put(question);
+    self.put_u32(ttl);
+    self.put_u16(data.len() as u16);
+    self.put(data);
+    let answer_count = u16::from_be_bytes([self[6], self[7]]) + 1;
+    self[6..8].copy_from_slice(&answer_count.to_be_bytes());
+  }
+}
+
+impl From<&[u8]> for Message {
+  fn from(value: &[u8]) -> Self {
+    Self(BytesMut::from(value))
+  }
+}
+
+impl Deref for Message {
+  type Target = BytesMut;
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for Message {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
+  }
+}
+
+impl Debug for Message {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    self.0.fmt(f)
+  }
+}
+
+pub fn encode_domain(name: &str) -> BytesMut {
+  name.split('.').flat_map(|label| once(label.len() as u8).chain(label.bytes())).chain(once(0u8)).collect()
+}
