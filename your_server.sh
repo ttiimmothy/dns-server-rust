a3b51bab0b669938cf7c4119b446f41e20e93721
ttiimmothy <dinosauli2006@gmail.com>
2023-12-07 09:21:43 -0500

    feat: rust

--- your_server.sh
+++ your_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 exec cargo run \
-    --quiet \
-    --release \
-    --target-dir=/tmp/codecrafters-dns-target \
-    --manifest-path $(dirname $0)/Cargo.toml -- "$@"
+  --quiet \
+  --release \
+  --target-dir=/tmp/codecrafters-dns-target \
+  --manifest-path $(dirname $0)/Cargo.toml -- "$@"
